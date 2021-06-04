7a70b33d287b6cb087f1e857758e9608441660ed
HENRY TRAN <45524711+henrytran5@users.noreply.github.com>
2021-06-04 12:37:39 -0500

    Update WEBCAM FACE DETECT WITH CV2.py

--- WEBCAM FACE DETECT WITH CV2.py
+++ WEBCAM FACE DETECT WITH CV2.py
@@ -18,7 +18,7 @@ while True:
         sleep(5)
         pass
 
-    # Capture frame-by-frame
+    # Capture frame by frame
     ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -30,7 +30,7 @@ while True:
         minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
+    # Draw a rectangle on the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
@@ -39,16 +39,16 @@ while True:
         log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
 
-    # Display the resulting frame
+    # Display a result of frame
     cv2.imshow('Video', frame)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # Display the resulting frame
+    # Display a result of frame
     cv2.imshow('Video', frame)
 
-# When everything is done, release the capture
+# Results is done, webcam show the capture
 video_capture.release()
 cv2.destroyAllWindows()
